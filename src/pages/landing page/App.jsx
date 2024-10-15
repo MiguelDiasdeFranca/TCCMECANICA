c3a26f6164df109f4577ee289099c37d6ba657eb
MiguelDiasdeFranca <migsx.07@gmail.com>
2024-10-15 18:06:12 -0300

    matato

--- src/pages/landing page/App.jsx
+++ src/pages/landing page/App.jsx
@@ -1,12 +1,24 @@
-import logoempresa from './images/logoemoresa.png';
-import './App.css';
+import logo from './logo.png'
+import './App.scss';
 
 export default function App() {
   return (
     <div className="App">
-      
+
+      <div className='cabecalho'>
+
+      <img src= {logo} className="logoempresa" alt="logo" />
+
+      <p>R. Serra de Japi, 1177 - Vila Gomes Cardim, São Paulo</p>
+      <p>(11) 93772-1364</p>
+
+      </div>
+
+
+
     </div>
   );
 }
 
 
+ 
