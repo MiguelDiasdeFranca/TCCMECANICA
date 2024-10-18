5bf9d16a845e4247119ff18c7c3418c661cb8965
GuilhermeMuniz10 <gm8074114@gmail.com>
2024-10-18 18:07:52 -0300

    fazer casaaaaaaaaaaaaaa

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './pages/landing page/App';
-import Administrador from './pages/adm/adm';
+import Login from './pages/login/login.jsx'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Login />
     
   </React.StrictMode>
 );
+
+
 
