5e861a51bbf6ca65ad356ec3eb94650e40ed1cf5
EduardoWendell <eduardowendellmoraes@gmail.com>
2024-10-21 15:47:54 -0300

    login

--- src/index.jsx
+++ src/index.jsx
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Login from './pages/login/login.jsx'
-import App from './pages/landingpage/App.jsx'
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <Administrador/>
-=======
-    
-<<<<<<< HEAD
-   <App/>
-=======
     <Login />
->>>>>>> 5bf9d16a845e4247119ff18c7c3418c661cb8965
->>>>>>> 131b0be23e7b8978488acfb805d14ee012314bde
+
     
   </React.StrictMode>
 );
--- src/pages/landingpage/App.jsx
+++ src/pages/landingpage/App.jsx
@@ -14,11 +14,6 @@ export default function App() {
         <a href="">sobre nós</a>
         <a href="">F.A.Q</a>
         <a href=""><button>faça seu agendamento</button></a>
-<<<<<<< HEAD:src/pages/landingpage/App.jsx
-=======
-
-
-
 
 
       </div>
@@ -44,11 +39,10 @@ export default function App() {
           <img className='imagemcarro' src={logo1} alt="logo1" />
           <h1>oiiiiiiii</h1>
         </div>
->>>>>>> 131b0be23e7b8978488acfb805d14ee012314bde:src/pages/landing page/App.jsx
+
       </div>
 
-      
-
+  
     </div>
   );
 }
