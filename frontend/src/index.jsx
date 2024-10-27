1d4c5636cb4d5f15a4b248a96304060e9382edc6
Eduardo <eduardowendellmoraes@gmail.com>
2024-10-27 16:25:41 -0300

    yoshaa

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Navigation from './routes';
-import Administrador from './pages/adm/adm';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <Administrador />
+    <Navigation />
   </React.StrictMode>
 )
