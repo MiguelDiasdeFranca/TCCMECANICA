0f556d37bd0e611f61a98d7060b00119d1e8b83d
odani01 <dr11072007@gmail.com>
2024-10-31 18:09:40 -0300

    000

--- frontend/src/pages/tech/index.jsx
+++ frontend/src/pages/tech/index.jsx
@@ -10,7 +10,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className='geral'>
       {/* Primeira parte: Formulário de contato */}
       <div className="container">
         <div className="left-side">
